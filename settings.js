666ea7741a17d0013bf9e6c8717e820fadb71af2
Akira YT Channel <97806782+AkiraUchiha92@users.noreply.github.com>
2022-06-26 19:17:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['6281327327914','6281327327914','6281327327914'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['6281327327914'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '6281327327914' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'ᴹᴿ᭄ AkiraOfcོ ×፝֟͜×' //ubah jadi nama mu, note tanda ' gausah di hapus!
   global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.footer = 'ᴹᴿ᭄ AkiraOfcོ ×፝֟͜×' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
+  global.youtube = 'https://youtube.com/c/AKIRAYTCHANNEL'
   global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-  global.myweb = 'https://api-riychdwayne.herokuapp.com'
+  global.myweb = 'https://AkiraOfficiallWA'
   global.region = 'indonesia'
 
   // Set Wm
   global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.author = 'ᴹᴿ᭄ AkiraOfcོ ×፝֟͜×' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
